e8c936d396cd7656a6d4dd710cc0c72574182a21
WillChen <will.chen@westpac.com.au>
2022-03-24 09:55:00 +1100

    more error samples

--- ErrorParser.py
+++ ErrorParser.py
@@ -19,34 +19,46 @@ class ErrorParser:
         self.lhs_current_topic = None
         self.rhs_message_input = None
 
-    def parse(self) -> None:
-        start = self.error.split("Incompatible schema")[-1]
-        elements = start.split("of type AVRO for schema")
-        for i, e in enumerate(elements):
-            elements[i] = elements[i].strip()
-        if len(elements) > 1:
-            while len(elements[0]) > 0 and not elements[0].endswith("}"):
-                elements[0] = elements[0][:-1]
-        self.lhs_current_topic = lhs_current_topic = eval(elements[0])
-        self.rhs_message_input = rhs_message_input = eval(elements[1])
-        pprint.pprint(lhs_current_topic, indent=1)
-        pprint.pprint(rhs_message_input, indent=1)
+    def parseStringBruteForce(self, s: str):
+        value = None
+        while len(s) and value is None:
+            try:
+                value = eval(s)
+            except:
+                s = s[:-1]
+        return value
 
-    def showDiff(self) -> None:
-        if self.lhs_current_topic is None or self.rhs_message_input is None:
-            self.parse()
+    def parse(self) -> None:
+
+        for start_match, mid_match in [["Incompatible schema", "of type AVRO for schema"],
+                                       ["Writers schema:", "Readers schema:"]]:
+            if start_match in self.error and mid_match in self.error:
+                start = self.error.split(start_match)[-1]
+                elements = start.split(mid_match)
+                for i, e in enumerate(elements):
+                    elements[i] = elements[i].strip()
+                if len(elements) > 1:
+                    while len(elements[0]) > 0 and not (elements[0].endswith("}") or elements[0].endswith("]")):
+                        elements[0] = elements[0][:-1]
+
+                self.lhs_current_topic = lhs_current_topic = self.parseStringBruteForce(elements[0])
+                self.rhs_message_input = rhs_message_input = self.parseStringBruteForce(elements[1])
+                pprint.pprint(lhs_current_topic, indent=1)
+                pprint.pprint(rhs_message_input, indent=1)
+
         with open("out/lhs_current_topic.json", "w") as flhs_current_topic:
             flhs_current_topic.write(json.dumps(self.lhs_current_topic, indent=1))
         with open("out/rhs_message_input.json", "w") as frhs_message_input:
             frhs_message_input.write(json.dumps(self.rhs_message_input, indent=1))
 
+    def showDiff(self) -> None:
         subprocess.run([
             "python", "pydiff/pydiff.py", "-p", "out/lhs_current_topic.json", "out/rhs_message_input.json"
         ])
 
-
 def main():
     ep = ErrorParser(file="input/error.log")
+    # ep.parse()
     ep.showDiff()
 
 
